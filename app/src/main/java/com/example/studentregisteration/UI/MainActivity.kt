272e3fd91dea49d912402223e662e1c4e2b29382
Irene Nyatichi <79199068+irenenyatichi@users.noreply.github.com>
2021-08-24 08:55:57 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/studentregisteration/UI/MainActivity.kt
+++ app/src/main/java/com/example/studentregisteration/UI/MainActivity.kt
@@ -1,109 +1,196 @@
-package com.example.studentregisteration.UI
+package com.example.registration.ui
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.renderscript.ScriptGroup
 import android.view.View
 import android.widget.*
-import com.example.registration.R
+import androidx.activity.viewModels
+import com.example.registration.Constants
+import com.example.registration.databinding.ActivityMainBinding
+import com.example.registration.models.LogInRequest
 import com.example.registration.models.RegistrationRequest
-import com.example.registration.ui.codeHiveRegistration
-import com.example.studentregisteration.userViewModel.UserViewModel
+import com.example.registration.viewmodel.UserViewModel
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var sharedPreferences: SharedPreferences
     val userViewModel: UserViewModel by viewModels()
 
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(ScriptGroup.Binding.root)
-        castViews()
-        onClick()
-    }
+        setContentView(binding.root)
 
-    fun setupSpinner() {
-        progressBar3=findViewById(R.id.progressBar3)
-        tilname = findViewById(R.id.tilname)
-        etdob = findViewById(R.id.etdob)
-        spnationality = findViewById(R.id.spnationality)
-        etpswd = findViewById(R.id.etpswd)
-        etphone = findViewById(R.id.etphone)
-        etemail = findViewById(R.id.etemail)
-        btn = findViewById(R.id.btn)
-        btnnext=findViewById(R.id.btnnext)
-        val nationality = arrayOf("Kenyan", "Ugandan", "Rwandan", "Sudanese", "South Sudanese")
-        val nationalityAdapter =
-                ArrayAdapter<String>(baseContext, android.R.layout.simple_spinner_item, nationality)
+        var nationality = arrayListOf<String>("Kenyan", "Ugandan", "Rwandese", "South Sudanes")
+        var nationalityAdapter =ArrayAdapter(baseContext, android.R.layout.simple_spinner_item, nationality)
+        binding.spNationality.adapter=nationalityAdapter
         nationalityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spnationality.adapter = nationalityAdapter
 
+        binding.btnLogIn1.setOnClickListener {
+            var intent = Intent(baseContext, LogIn::class.java)
+            startActivity(intent)
+        sharedPreferences = getSharedPreferences(Constants.SHAREDPREFS, Context.MODE_PRIVATE)
+            redirectUser()
+        }
     }
 
-    fun onClick() {
-        btn.setOnClickListener {
-            progressBar3.visibility=View.VISIBLE
-            var name = tilname.text.toString()
-            var dob = etdob.text.toString()
-            var nationality = spnationality.selectedItem.toString().uppercase()
-            var password = etpswd.text.toString()
-            var phone = etphone.text.toString()
-            var email = etemail.text.toString()
-            if(name.isEmpty()||dob.isEmpty()||password.isEmpty()||phone.isEmpty()||email.isEmpty()){
-                tilname.setError("please enter your name")
-                etdob.setError("Please enter your date of birth")
-                etpswd.setError("You must enter you ID number")
-                etphone.setError("please enter your phone number")
-                etemail.setError("please enter your email")
-                Toast.makeText(baseContext,"This are your details :",Toast.LENGTH_LONG).show()
-            }
-            var intent=Intent(baseContext, codeHiveRegistration::class.java)
-            startActivity(intent)
-            var regRequest=RegistrationRequest(
-                    name=name,phoneNumber = phone,email = email,dateOfBirth = dob,nationality = nationality,password = password
-            )
+    fun redirectUser(){
+        var accessToken = sharedPreferences.getString(Constants.ACCESSTOKEN, Constants.EMPTY_STRING)
+        if (accessToken!!.isNotEmpty()){
+            startActivity(Intent(baseContext, CoursesActivity::class.java))
+        }
+        else{
+            startActivity(Intent(baseContext, LogIn::class.java))
+        }
+    }
 
-            userViewModel.registerUser(regRequest)
-//            var retrofit=ApiClient.buildApiClient(ApiInterface::class.java)
-//            var request=retrofit.registerStudent(regRequest)
-//            request.enqueue(object : Callback<RegistrationResponse> {
-//                override fun onResponse(call: Call<RegistrationResponse>, response: Response<RegistrationResponse>) {
-//                    progressBar3.visibility=View.GONE
-//                    if (response.isSuccessful){
-//                        Toast.makeText(baseContext,"Your registration is successful",Toast.LENGTH_LONG).show()
-//                    }
-//                    else{
-//                        Toast.makeText(baseContext,response.errorBody()?.toString(),Toast.LENGTH_LONG).show()
+
+    //Read on android activity life cycle
+    override fun onResume() {
+        super.onResume()
+        binding.btnRegister.setOnClickListener{
+            if (binding.etName.text.toString().isEmpty()||
+                binding.etDob.text.toString().isEmpty()||
+                binding.etPhoneNumber.text.toString().isEmpty()||
+                binding.etEmail.text.toString().isEmpty() ||
+                binding.etPassword.text.toString().isEmpty()
+            ){
+                binding.etName.setError("Name required")
+                binding.etDob.setError("Date of birth required")
+                binding.etPhoneNumber.setError("Number required")
+                binding.etEmail.setError("Email required")
+                binding.etPassword.setError("Password required")
+
+            }
+
+            var regRequest = RegistrationRequest(
+                name = binding.etName.text.toString(),
+                phoneNumber = binding.etPhoneNumber.text.toString(),
+                email = binding.etEmail.text.toString(),
+                dateOfBirth = binding.etDob.text.toString(),
+                password = binding.etPassword.text.toString(),
+                nationality =binding.spNationality.selectedItem.toString().uppercase()
+            )
+            var intent = Intent(baseContext, LogIn::class.java)
+            startActivity(intent)
+
+            userViewModel.registerStudent(regRequest)
+        }
+        userViewModel.registrationLiveData.observe(this, { regResponse->
+            if (!regResponse.studentId.isNullOrEmpty()){
+                Toast.makeText(baseContext, "Registration successful", Toast.LENGTH_LONG).show()
+            }
+        })
+        userViewModel.regFailedLiveData.observe(this, { error->
+            Toast.makeText(baseContext, error, Toast.LENGTH_LONG).show()
+        })
+    }
+
+
+
+
+//        castViews()
+//        clickRegister()
+
+//    lateinit var etName: EditText
+//    lateinit var etDob: EditText
+//    lateinit var spNationality: Spinner
+//    lateinit var etPhoneNumber: EditText
+//    lateinit var etEmail: EditText
+//    lateinit var btnRegister: Button
+//    lateinit var etPassword: EditText
+
+//var name = binding.etName.text.toString()
+//var Dob = binding.etDob.text.toString()
+//var phoneNumber = binding.etPassword.text.toString()
+//var email = binding.etEmail.text.toString()
+//var password = binding.etPassword.text.toString()
+
+
+//    fun castViews(){
+//        etName = findViewById(R.id.etName)
+//        etDob  = findViewById(R.id.etDob)
+//        spNationality = findViewById(R.id.spNationality)
+//        etEmail = findViewById(R.id.etEmail)
+//        etPhoneNumber = findViewById(R.id.etPhoneNumber)
+//        btnRegister = findViewById(R.id.btnRegister)
+//        etPassword = findViewById(R.id.etPassword)
+
+//        val nationality = arrayOf("Kenyan", "Ugandan", "Rwandese", "South Sudanes")
+//        var nationalityAdapter = ArrayAdapter<String>(baseContext, android.R.layout.simple_spinner_item, nationality)
+//        nationalityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//        spNationality.adapter = nationalityAdapter
 //
+//    }
+//    fun clickRegister(){
+//        var error = false
+//        btnRegister.setOnClickListener {
+//            var name = etName.text.toString()
+//            if (name.isEmpty()) {
+//                error = true
+//                etName.setError("Name is required")
+//            }
+
+//            var dob = etDob.text.toString()
+//            if (dob.isEmpty()) {
+//                error = true
+//                etDob.setError("Date of birth is required")
+//            }
+//
+//            var tilPassword = etPassword.text.toString()
+//            if (tilPassword.isEmpty()) {
+//                error = true
+//                etPassword.setError("Input password")
+//            }
+//
+//            var nationality = spNationality.selectedItem.toString()
+//
+//            var email = etEmail.text.toString()
+//            if (email.isEmpty()) {
+//                error = true
+//                etEmail.setError("Email required")
+//            }
+//
+//            var phoneNumber = etPhoneNumber.text.toString()
+//            if (phoneNumber.isEmpty()) {
+//                error = true
+//                etPhoneNumber.setError("Input phone number")
+//            }
+//
+//            var registrationRequest = RegistrationRequest(
+//                name = name,
+//                phoneNumber = phoneNumber,
+//                email = email,
+//                dateOfBirth = dob,
+//                password = tilPassword
+//            )
+//
+//            val retrofit = ApiClient.buildApiClient(ApiInterface::class.java)
+//            var request = retrofit.registerStudent(registrationRequest)
+//            request.enqueue(object : Callback<RegistrationResponse> {
+//
+//                override fun onResponse(
+//                    call: Call<RegistrationResponse>,
+//                    response: Response<RegistrationResponse>
+//                ) {
+//                    if (response.isSuccessful) {
+//                        Toast.makeText(baseContext, "Registration successful", Toast.LENGTH_LONG)
+//                            .show()
 //                    }
 //                }
 //
 //                override fun onFailure(call: Call<RegistrationResponse>, t: Throwable) {
-//                    Toast.makeText(baseContext,t.message,Toast.LENGTH_LONG).show()
-//                    progressBar3.visibility=View.GONE
-//
-//
+//                    Toast.makeText(baseContext, t.message, Toast.LENGTH_LONG).show()
 //                }
+//
 //            })
-        }
-        btnnext.setOnClickListener {
-            var intent=Intent(baseContext, Loginctivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        userViewModel.regResponseLiveData.observe(this,{regResponse ->
-            binding.pbRegisteration.visibility = View.GONE
-            if (!regResponse.studentId.isNullOrEmpty()){
-                Toast.makeText(baseContext,"Registeration Successful",Toast.LENGTH_LONG).show()
-            }
-        })
-        userViewModel.regErrorLiveData.observe(this,{error ->
-            Toast.makeText(baseContext,"Registeration Successful",Toast.LENGTH_LONG).show()
-        })
-
-    }
+//            val intent = Intent(baseContext, LogIn::class.java)
+//            startActivity(intent)
+//        }
+//}
 }
