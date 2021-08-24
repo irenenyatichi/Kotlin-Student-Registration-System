3f18afd2481745c730519cc48e5d9e5e42c0dcbf
Irene Nyatichi <79199068+irenenyatichi@users.noreply.github.com>
2021-08-24 08:40:17 +0300

    Update RegisterationRequest.kt

--- app/src/main/java/com/example/studentregisteration/Models/RegisterationRequest.kt
+++ app/src/main/java/com/example/studentregisteration/Models/RegisterationRequest.kt
@@ -3,11 +3,10 @@ package com.example.registration.models
 import com.google.gson.annotations.SerializedName
 
 data class RegistrationRequest(
-    var name:String,
-    @SerializedName("phone_number")var phoneNumber:String,
-    var email:String,
-    var password:String,
-    var nationality:String,
-    @SerializedName("date_of_birth") var dateOfBirth:String,
-    @SerializedName("student_id") var studentId:String,
+    var name: String,
+    @SerializedName("phone_number") var phoneNumber: String,
+    var email: String,
+    @SerializedName("date_of_birth") var dateOfBirth: String,
+    var password: String,
+    var nationality: String
 )
