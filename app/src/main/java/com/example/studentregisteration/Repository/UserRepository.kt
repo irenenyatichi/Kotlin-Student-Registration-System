bd3f2ecd30c87fb2f4d3bb3c191e6651689412ae
Irene Nyatichi <79199068+irenenyatichi@users.noreply.github.com>
2021-08-24 08:44:11 +0300

    Update UserRepository.kt

--- app/src/main/java/com/example/studentregisteration/Repository/UserRepository.kt
+++ app/src/main/java/com/example/studentregisteration/Repository/UserRepository.kt
@@ -1,10 +1,27 @@
-package com.example.studentregisteration.Repository
+package com.example.registration.repository
 
-import com.example.studentregisteration.Api.ApiClient
-import com.example.studentregisteration.Api.ApiInterface
+import com.example.registration.api.ApiClient
+import com.example.registration.api.ApiInterface
+import com.example.registration.models.LogInRequest
+import com.example.registration.models.LogInResponse
+import com.example.registration.models.RegistrationRequest
+import com.example.registration.models.RegistrationResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
 
 class UserRepository {
-    var retrofit = ApiClient.buildApiClient(ApiInterface::class.java)
+    var apiInterface = ApiClient.buildApiClient(ApiInterface::class.java)
 
-    suspend fun registerUser()
+    suspend fun registerStudent(registrationRequest: RegistrationRequest):
+            Response<RegistrationResponse> = withContext(Dispatchers.IO){
+                var response = apiInterface.registerStudent(registrationRequest)
+                    return@withContext response
+    }
+
+    suspend fun login(logInRequest: LogInRequest):Response<LogInResponse> =
+        withContext(Dispatchers.IO){
+            var response = apiInterface.logInStudent(logInRequest)
+            return@withContext response
+        }
 }
