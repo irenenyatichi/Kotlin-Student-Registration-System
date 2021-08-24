88cf086c8a0d5fd227d09c23a7b4832152478742
Irene Nyatichi <79199068+irenenyatichi@users.noreply.github.com>
2021-08-24 09:00:28 +0300

    Create Constants.kt

---
+++ app/src/main/java/com/example/studentregisteration/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.registration
+
+class Constants{
+    companion object{
+        const val SHAREDPREFS = "CODEHIVE_REG_PREFS"
+        const val ACCESSTOKEN = "ACCESS_TOKEN"
+        const val STUDENTID = "STUDENT_ID"
+        const val EMPTY_STRING = "EMPTY_STRING"
+    }
+}
