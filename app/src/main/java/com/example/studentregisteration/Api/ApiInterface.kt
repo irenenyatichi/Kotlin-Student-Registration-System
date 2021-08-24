967af7f4525f40d03a62ba5087ca474f3ef15f6b
Irene Nyatichi <79199068+irenenyatichi@users.noreply.github.com>
2021-08-24 08:23:03 +0300

    Update ApiInterface.kt

--- app/src/main/java/com/example/studentregisteration/Api/ApiInterface.kt
+++ app/src/main/java/com/example/studentregisteration/Api/ApiInterface.kt
@@ -1,12 +1,22 @@
-package com.example.studentregisteration.Api
+package com.example.registration.api
 
-import com.example.registration.models.RegistrationRequest
-import com.example.registration.models.RegistrationResponse
-import retrofit2.Call
+import com.example.registration.models.*
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiInterface {
-    @POST("/student/register")
-    suspend fun registerStudent(@Body registrationRequest: RegistrationRequest):<RegistrationResponse>
+    @POST("/students/register")
+    suspend fun registerStudent(@Body registrationRequest: RegistrationRequest): Response<RegistrationResponse>
+
+    @POST("/students/login")
+    suspend fun logInStudent(@Body logInRequest: LogInRequest): Response<LogInResponse>
+
+    @GET("/courses")
+    suspend fun studentCourses(@Header("Authorization") token: String): Response<List<CoursesResponse>>
+
+    @POST("/enrolments")
+    suspend fun enrol(@Header("Authorization") token: String): Response<EnrolmentResponse>
 }
